4ba2646000c54d3eec5927cf54c85fe85d2d8566
Gaurav Chitroda <gaurav.chitroda@appfolio.com>
2020-03-07 10:36:27 -0800

    Initial Commit

---
+++ EmployeeDirectory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmployeeDirectory
+//
+//  Created by Gaurav Chitroda on 3/7/20.
+//  Copyright © 2020 Gaurav Chitroda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
